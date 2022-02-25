33feb720d596491c9bbef3620437547b3bcc7ec2
NikolayKharkov <vtiko564@gmail.com>
2022-02-25 15:53:52 +0300

    2. ManyToMany [#331986]

--- src/main/java/many/HbmRun.java
+++ src/main/java/many/HbmRun.java
@@ -1,5 +1,7 @@
 package many;
 
+import models.Author;
+import models.Book;
 import models.CarBrand;
 import models.CarModel;
 import org.hibernate.Session;
@@ -11,10 +13,16 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class HbmRun {
 
     public static void main(String[] args) {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure().build();
+        HbmRun hbmRun = new HbmRun();
+        hbmRun.runManyToMany();
+    }
+
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+
+    public void runOneToMany() {
         try {
-            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
             Session session = sf.openSession();
             session.beginTransaction();
 
@@ -35,4 +43,37 @@ public class HbmRun {
             StandardServiceRegistryBuilder.destroy(registry);
         }
     }
+
+    public void runManyToMany() {
+        try {
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            Author author1 = new Author("Author_1");
+            Author author2 = new Author("Author_2");
+            Author author3 = new Author("Author_3");
+            Book book1 = new Book("Book_1");
+            Book book2 = new Book("Book_2");
+            Book book3 = new Book("Book_3");
+            Book book4 = new Book("Book_4");
+            author1.addBook(book1);
+            author1.addBook(book2);
+            author2.addBook(book1);
+            author2.addBook(book4);
+            author3.addBook(book3);
+            session.persist(author1);
+            session.persist(author2);
+            session.persist(author3);
+            Author author1FromDb = session.find(Author.class, 1);
+            session.remove(author1FromDb);
+
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
 }
---
+++ src/main/java/models/Author.java
@@ -0,0 +1,71 @@
+package models;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Book> books = new HashSet<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Author() {
+
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Author author = (Author) o;
+        return id == author.id && Objects.equals(name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + '}';
+    }
+}
+
